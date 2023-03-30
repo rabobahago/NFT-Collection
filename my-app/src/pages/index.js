e0445c6d136df01489811edff7da44816ac4c072
rabobahago <rabobahago@gmail.com>
2023-03-30 17:19:23 +0100

    render part of home component created

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -11,12 +11,11 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
   const [isOwner, setIsOwner] = useState(false);
   const [tokenIdsMinted, setTokenIdsMinted] = useState(0);
-  const web3modalRef = ref();
+  const web3modalRef = useRef();
   const getProviderOrSigner = async (needSigner = false) => {
     const provider = await web3modalRef.current.connect();
     const web3Provider = new providers.Web3Provider(provider);
     const { chainId } = await web3Provider.getNetwork();
-    console.log(chainId);
     if (chainId !== 5) {
       window.alert("Change the network to Goerli");
       throw new Error("Change network to Goerli");
@@ -27,8 +26,44 @@ export default function Home() {
     }
     return web3Provider;
   };
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
   const presaleMint = async () => {
     try {
     } catch (err) {}
   };
+  return (
+    <div>
+      <Head>
+        <title>Crypto Devs</title>
+        <meta name="description" content="Whitelist-Dapp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to Crypto Devs!</h1>
+          <div className={styles.description}>
+            It&#39;s an NFT collection for developers in Crypto.
+          </div>
+          <div className={styles.description}>
+            {tokenIdsMinted}/20 have been minted
+          </div>
+          {/* {renderButton()} */}
+        </div>
+        <div>
+          <img className={styles.image} src="0.svg" />
+        </div>
+      </div>
+
+      <footer className={styles.footer}>
+        Made with &#10084; by Crypto Rabo
+      </footer>
+    </div>
+  );
 }
