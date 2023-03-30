e0a6c60b294841d0190f9316965f5e89e5c462cd
rabobahago <rabobahago@gmail.com>
2023-03-30 13:19:45 +0100

    deploy scripts added

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "address-of-the-whitelist-contract";
+const WHITELIST_CONTRACT_ADDRESS = "0xe49Ee8cc8A39422994414f5A867F83f7CE05Bf38";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 
