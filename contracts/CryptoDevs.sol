4d2f2f6677047bc74ffda703e99113a79a5997c4
rabobahago <rabobahago@gmail.com>
2023-03-30 12:25:34 +0100

    mint function implemented

--- contracts/CryptoDevs.sol
+++ contracts/CryptoDevs.sol
@@ -67,4 +67,18 @@ contract CryptoDevs is Ownable, ERC721Enumerable {
         // If the address being minted to is not a contract, it works the same way as _mint
         _safeMint(msg.sender, tokenIds);
     }
+
+    /**
+     * @dev mint allows a user to mint 1 NFT per transaction after the presale has ended.
+     */
+    function mint() public payable onlyWhenOwnerNotPaused {
+        require(
+            presaleStarted && block.timestamp >= presaleEnded,
+            "Presale has not ended yet"
+        );
+        require(tokenIds < maxTokenIds, "Exceed maximum Crypto Devs supply");
+        require(msg.value >= _price, "Ether sent is not correct");
+        tokenIds += 1;
+        _safeMint(msg.sender, tokenIds);
+    }
 }
