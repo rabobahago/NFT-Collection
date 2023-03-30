b9a800a4674a1ee973e46accf99012f035871454
rabobahago <rabobahago@gmail.com>
2023-03-30 13:17:26 +0100

    hardhat config and constants file added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const ALCHEMY_URL = process.env.ALCHEMY_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: ALCHEMY_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
